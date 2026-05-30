688d7f6b9f5d0709b980e63ec5a4f8b48abc5113
Potta Sai Varshith <144475980+saivarshith67@users.noreply.github.com>
2026-05-30 16:11:53 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,9 @@
+from functools import lru_cache
+
+class Solution:
+    @lru_cache
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
